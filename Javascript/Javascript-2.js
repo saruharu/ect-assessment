842a559df002ab0a7f134d72685e30f7755807a3
saruharu <saruma.san1@gmail.com>
2022-06-06 20:27:56 +0100

    javascript solutions update

--- Javascript/Javascript-2.js
+++ Javascript/Javascript-2.js
@@ -1,25 +1,42 @@
-
-
-  function check(obj1,obj2){
-    let i =0
-    for(let key of Object.keys(obj1)){
-        i = i+1
-        if(!(obj2[key])){
-            return false;
-        }
-        if(obj1.key!==obj2.key) return false;
-    }
-    return true;
+//FUNCTION
+function check(obj1,obj2){
+  [obj1,obj2] = [obj1,obj2].map(obj=>removeNull(obj)).map(obj=>JSON.stringify(obj));
+  return obj1===obj2;
 }
 
 
-var data1 = { a: 17, b: { c: 'Test', d: null } };
-var data2 = { a: 17, b: { c: 'Test' } };
+//clean the objects from null and undefined (flat & nested) elements
+removeNull = (obj) => {
+  Object.keys(obj).forEach(key =>
+    (obj[key] && typeof obj[key] === 'object') && removeNull(obj[key]) ||
+    (obj[key] === undefined || obj[key] === null) && delete obj[key]  ||
+    (obj[key] && Array.isArray(obj) && obj.clean(undefined))
+
+  );
+  return obj;
+};
+
+//clean empty/undefined elements from arrays
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {         
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
+
+
+//TEST
+var data1 = { a: 17, b: { c: 'Test', d: null }, e: [45,20,null], f: new Date('02-25-2022'), g: true};
+var data2 = { a: 17, b: { c: 'Test' },  e: [45,20], f: new Date('02-25-2022'), g: true};
 var data3 = { a: 17, b: null };
+var data4 = { a: 17, b: { c: 'Test' },  e: [45,25], f: new Date('02-10-2022')};
+
 console.log(check(data1, data2));
 console.log(check(data1, data3)); 
+console.log(check(data2, data4)); 
+
   
   
-  const clone3 = window.structuredClone(a)
-  console.log(deepCopy(a));
-  console.log(typeof deepCopy(a).date);  // result of .toISOString()
--- Javascript/Javascript-3.js
+++ Javascript/Javascript-3.js
@@ -1,14 +1,28 @@
+var $math = {
+    sum: (a,b)=>{return a+b},
+    mul: (a,b)=>{return a*b}
+};
+
+var $logger = {
+    call: (text,result)=>{return console.log(text,result)}
+}
+
+//FUNCTION
 function execute(code, variables) {
     if(code.includes('$math.sum')){
-        return console.log("Sum: ", Object.values(variables).reduce((a,b) => (b + a)));
+        return $logger.call("Sum:", Object.values(variables).reduce((a,b) => $math.sum(a,b)));
     }
     if(code.includes('$math.mul')){
-        return console.log("Mul: ", Object.values(variables).reduce((a,b) => (b * a)));
-        
+        return $logger.call("Mul:", Object.values(variables).reduce((a,b) => $math.mul(a,b)));
     }
-    }
-    
-    execute('$logger("Sum:", $math.sum(a, b))', { a: 17, b: 3, c: 4});
-    // returns Sum: 20
-    execute('$logger("Mul:", $math.mul(a, b))', { a: 17, b: 3 });
-    // returns Mul: 51
+}
+
+
+
+//TEST
+execute('$logger("Sum:", $math.sum(a, b))', { a: 17, b: 3});
+// returns Sum: 20
+execute('$logger("Mul:", $math.mul(a, b))', { a: 17, b: 3});
+// returns Mul: 51
+execute('$logger("Mul:", $math.mul(a, b, c))', { a: 17, b: 3, c: 12});
+// returns Mul: 612
